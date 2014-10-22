e71a9f09a4ccb057ecf56e1659acf4c7c94f5912
eh2be <eh2be@users.noreply.github.com>
2014-10-22 23:33:48 +0000

    login done

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,21 @@
 get '/' do
   erb :index
 end
+
+get '/login' do
+  erb :signin
+end
+
+post '/login' do
+  username = params[:username]
+  pw = params[:password]
+  user = User.where(username: username, password: password)
+
+  if user != []
+    session[:username] = username
+
+    redirect '/stories'
+  else
+    redirect '/login'
+  end
+end
