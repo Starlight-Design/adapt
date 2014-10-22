3db027c17a683fecca9fd9817eeb42c8340e0a2f
eh2be <eh2be@users.noreply.github.com>
2014-10-22 19:39:45 +0000

    nothing done

--- app/actions.rb
+++ app/actions.rb
@@ -2,3 +2,39 @@
 get '/' do
   erb :index
 end
+
+get '/user_sessions/new' do
+  erb :index
+end
+
+post '/user_sessions/new' do
+  username = params[:username]
+  pw = params[:password]
+  user = User.where(username: username, password: pw)
+
+  if user != []
+    session[:username] = username
+    redirect '/stories'
+  else
+    redirect '/login'
+  end
+end
+
+# get '/stories' do
+#   erb :index
+# end
+
+# get '/stories/new' do
+# end
+
+# get '/comments/new' do
+# end
+
+# get '/users' do
+# end
+
+# get '/users/new' do
+# end
+
+
+
