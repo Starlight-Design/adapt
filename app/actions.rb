5cecd794b9e8c54ab2f2a3a776038fe893539621
nvanzant <nick@fullroster.ca>
2014-10-22 23:14:54 +0000

    enabled session, created register and log out

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,31 @@
 # Homepage (Root path)
+enable :sessions
+
+helpers do
+  def current_user
+    current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+end
+
 get '/' do
   erb :index
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/register' do
+  user = User.create(user_name: params[:user_name], email: params[:email], password: params[:password])
+  if user
+    session[:user_id] = user.id
+    redirect '/stories'
+  else
+    redirect '/register'
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect '/'
 end
