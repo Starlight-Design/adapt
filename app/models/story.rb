b5e469c306ed6bf96054c51d4afa999ce3eec464
nvanzant <nick@fullroster.ca>
2014-10-23 04:26:16 +0000

    heres some shit working towards getting upvotes/downvotes to work

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,4 +3,15 @@ class Story < ActiveRecord::Base
 	has_many :comments
 	has_many :steps
 	has_many :story_upvotes
+	# has_many :story_downvotes
+
+ #  def upvote
+ #    self.score += 1
+ #    self.save
+ #  end
+
+ #  def downvote
+ #    self.score -= 1
+ #    self.save
+ #  end
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $("form"); //Fields wrapper
+    var add_button      = $(".stepping"); //Add button ID
+    
+    var x = 1; //initlal text box count
+    $(add_button).click(function(e){ //on add input button click
+        e.preventDefault();
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            $(wrapper).append('<div class="form-group col-md-8 col-md-offset-3"><label for="step_text">Steps</label><input type="text" class="width_control form-control"  id="step_text" placeholder="Enter Song title" name="step_text" value="<%= @story && @story.step_text %>"></div>'); //add input box
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).parent('div').remove(); x--;
+    })
 });
+  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
